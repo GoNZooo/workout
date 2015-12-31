13ce9bef4856b3772703d8b43a122c60284ad087
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-31 20:43:47 +0200

    Add 'log' & 'add' command placeholders

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	MIX_ENV=prod mix escript.build
--- lib/workout/cli.ex
+++ lib/workout/cli.ex
@@ -1,6 +1,8 @@
 defmodule Workout.CLI do
   def main(argv) do
-    argv |> parse_args |> process_args
+    argv
+    |> parse_args
+    |> process_args
   end
 
   def parse_args(argv) do
@@ -19,10 +21,23 @@ defmodule Workout.CLI do
   end
 
   def process_args({[help: true], _, _}) do
-    IO.puts "workout [--help/-h] --distance/-d D.D --time/-t T.T --gear/-g G"
+    IO.puts "workout [--help/-h] --distance/-d D.D --time/-t T.T --gear/-g G=3"
   end
 
-  def process_args({[], _, _}) do
-    IO.puts "workout [--help/-h] --distance/-d D.D --time/-t T.T --gear/-g G"
+  def process_args({[], [], _}) do
+    IO.puts "workout [--help/-h] --distance/-d D.D --time/-t T.T --gear/-g G=3"
+  end
+
+  def process_args({[distance: d, time: t, gear: g], ["add"], _}) do
+    IO.puts "Adding: distance: #{d}, time: #{t}, gear: #{g}"
+  end
+
+  def process_args({[distance: d, time: t], ["add"], _}) do
+    g = 3
+    IO.puts "Adding: distance: #{d}, time: #{t}, gear: #{g}"
+  end
+
+  def process_args({_, ["log"], _}) do
+    IO.puts "Outputting log"
   end
 end
