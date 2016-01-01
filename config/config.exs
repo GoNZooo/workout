bee28a9406941a16ee8588d4de504f6f4e0ee975
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-01 13:40:14 +0200

    Add config entry for how to locate log file

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,5 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :workout, log_file: System.user_home! <> "/.local/share/workout/data/log"
