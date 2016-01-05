bb92e3331bc52258a163749bcacf9f2a491b07d3
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-05 23:39:38 +0200

    Add scoring for sessions

--- lib/workout/log.ex
+++ lib/workout/log.ex
@@ -9,6 +9,11 @@ defmodule Workout.Log do
     Float.round(distance / (time / 60), precision)
   end
 
+  defp calculate_score(average_speed, time, precision \\ 2) do
+    score = (:math.log2(average_speed) + :math.log2(1/10)) * time
+    Float.round(score, precision)
+  end
+
   defp output_line("\n"), do: :ok
 
   defp output_line(line) do
@@ -16,10 +21,11 @@ defmodule Workout.Log do
     time = parse(:float, time)
     distance = parse(:float, distance)
     average_speed = calculate_average_speed(distance, time)
+    score = calculate_score(average_speed, time)
 
     IO.write "#{IO.ANSI.green}#{date}#{IO.ANSI.reset}: "
     IO.write "#{distance} km in #{time} minutes on gear #{gear} "
-    IO.puts "(~#{average_speed} km/h)"
+    IO.puts "(~#{average_speed} km/h, score: #{score})"
   end
 
   def append_entry(date, time, distance, gear) do
@@ -46,17 +52,22 @@ defmodule Workout.Log do
     file
     |> Stream.map(&(String.split(&1, ";")))
     |> Stream.map(fn [_, t, d, _] -> {parse(:float, t), parse(:float, d)} end)
-    |> Enum.reduce({0, 0}, fn {t, d}, {tacc, dacc} -> {t+tacc, d+dacc} end)
+    |> Enum.reduce({0, 0, 0},
+      fn {t, d}, {tacc, dacc, nacc} ->
+        {t+tacc, d+dacc, nacc + 1}
+      end)
   end
 
   def sum_fields(:all) do
     f = File.stream!(Application.get_env(:workout, :log_file))
-    {total_time, total_distance} = sum_file(f)
+    {total_time, total_distance, entries} = sum_file(f)
     average_speed = calculate_average_speed(total_distance, total_time)
+    average_score = Float.round(calculate_score(average_speed, total_time) / entries, 2)
 
     IO.puts ""
     IO.puts("#{IO.ANSI.magenta}Total time: #{total_time} minutes")
     IO.puts("Total distance: #{total_distance} km")
     IO.puts("Average speed: #{average_speed} km/h")
+    IO.puts("Average score: #{average_score}")
   end
 end
